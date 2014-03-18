92236eba23405b3b496e983045954e1bc3949406
Vadym <ghoast@ukr.net>
2014-03-18 14:56:11 +0200

    implemented new version
    now works only with int and types inherited from Base Class

--- main.cpp
+++ main.cpp
@@ -9,106 +9,76 @@ using namespace std;
 ////////////////////////////////////////////////////////////////////////////////
 // some additional macro Class
 
-#define SAVE(Obj) Obj() {
-#define REG_MEMBER(type, x) \
-    RegisterMBS(new ValueHolderType<type>(&x, #x))
+#define SAVE void RegisterAllMBS(list<int *>& aList) {
+#define SAVE2(Parent) SAVE Parent::RegisterAllMBS(aList);
+#define REG_MEMBER(type, x) RegisterOneMBR(aList, &x)
 #define ENDSAVE }
-
-////////////////////////////////////////////////////////////////////////////////
-// ValueHolder Class
-
-class ValueHolder {
-public:
-    virtual void loadVal() = 0;
-    virtual void saveVal() = 0;
-    virtual ~ValueHolder() {}
-};
-
-template <typename type>
-class ValueHolderType : public ValueHolder {
-public:
-    ValueHolderType(type* val, const char valN[]) : value(val), valName(valN) {}
-
-    void loadVal() {
-        cout << "Please enter " << valName << ": ";
-        cin >> *value;
-    }
-
-    void saveVal() {
-        cout << "<" << valName << "> " << *value;
-        cout << " </" << valName << ">" << endl;
-        }
-
-private:
-    type *value;
-    const char *valName;
-};
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // Base Class
 
-list<ValueHolder *> _G_members;
-
-typedef  list<ValueHolder *>::iterator ListIter;
-
 class BaseClass {
 public:
-    void Load(const char *fileName)
-    {
-        auto mmb = members;
-        for (auto cnt = members_count; cnt > 0; cnt--, mmb++) {
-            (*mmb)->loadVal(); //@todo: implement correct loadVal
-        }
+    void Load(const char *fileName) {
+        init_members();
+
+        for (auto &mbr : members)
+            cin >> *mbr;
     }
-    void Save(const char *fileName)
-    {
-        auto mmb = members;
-        for (auto cnt = members_count; cnt > 0; cnt--, mmb++) {
-            (*mmb)->saveVal(); //@todo: implement correct saveVal
-        }
-    }
+    void Save(const char *fileName) {
+        init_members();
 
-    BaseClass() : members_count (0) {}
-
-    void RegisterMBS(ValueHolder * mbs)
-    {
-        if (members_count)
-            _G_members.push_back(mbs);
-        else
-            members = _G_members.insert(_G_members.end(), (mbs));
-
-        members_count++;
+        for (auto &mbr : members)
+            cout << *mbr << endl;
+        cout << endl;
     }
 
-    virtual ~BaseClass()
-    {
-        auto mmb = members;
-        for (auto cnt = members_count; cnt > 0; cnt--) {
-            delete (*mmb);
-            _G_members.erase(mmb++);
-        }
+    BaseClass() : members(0), is_inited(false) {}
+    BaseClass(const BaseClass & bc) : members(0), is_inited(false) {}
+
+    BaseClass & operator=(BaseClass & that) {
+        if (this == &that)
+            return *this;
+        
+        is_inited = false;
+        members.clear();
     }
+
+    virtual void RegisterAllMBS(list<int *>& aList) = 0;
+
+    void RegisterOneMBR(list<int *>& aList, int *mbr) {
+        aList.push_back(mbr);
+    }
+
+    void RegisterOneMBR(list<int *>& aList, BaseClass *mbr) {
+        mbr->RegisterAllMBS(aList);
+    }
+    
+    virtual ~BaseClass();
+
 private:
-    ListIter members;
-    int members_count;
+    list<int *> members;
+    bool      is_inited;
+
+    void init_members() {
+        if (!is_inited)
+            RegisterAllMBS(members);
+        is_inited = true;
+    }
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 // Point Class
 
-class Point: public BaseClass{
+class Point : public BaseClass{
 public:
     int x;
-    float y;
-    double z;
-    string name;
+    int y;
+    int z;
 
-    SAVE(Point)
-        REG_MEMBER(int,x);
-        REG_MEMBER(float,y);
-        REG_MEMBER(double,z);
-        REG_MEMBER(string,name);
+    SAVE
+        REG_MEMBER(int, x);
+        REG_MEMBER(int, y);
     ENDSAVE;
 };
 
@@ -119,28 +89,55 @@ class Point4D : public Point
 public:
     int v;
 
-    SAVE(Point4D)
-        REG_MEMBER(int,v);
+    SAVE2(Point)
+        REG_MEMBER(int, v);
     ENDSAVE;
 };
 
+////////////////////////////////////////////////////////////////////////////////
+// Point4D Class v2
+class Point4D_v2 : public BaseClass
+{
+public:
+    Point p;
+    int v;
+
+    SAVE
+        REG_MEMBER(Point, p);
+        REG_MEMBER(int, v);
+    ENDSAVE;
+};
 ////////////////////////////////////////////////////////////////////////////////
 // Main
 
 int main()
 {
     Point p;
-    p.Load("point.xml");
-    p.x = 10;
-    p.Save("point2.xml");
-
-    {
-        Point p2;
-    }
-
-    Point4D p2;
-    p2.Load("");
+    p.Load("");
+    p.Save("");
+    
+    Point p2 = p;
+    p2.x = 2;
+    p2.y = 2;
     p2.Save("");
+
+    Point p3;
+    p3 = p;
+    p3.x = 3;
+    p3.Save("");
+
+    Point4D p4;
+    p4.Load("");
+    p4.Save("");
+
+
+    Point4D p4_copy = p4;
+    p4_copy.x = 2222;
+    p4_copy.Save("");
+
+    Point4D_v2 p4_v2;
+    p4_v2.Load("");
+    p4_v2.Save("");
 
     return 0;
 }
